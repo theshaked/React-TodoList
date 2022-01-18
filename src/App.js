febdf59ed5d8d4481eaef0a6c6259efd5f49cbad
Shaked Sibony <shaked2009@gmail.com>
2022-01-18 06:44:58 -0800

    New Tasks component added using state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
  import Header from "./components/Header";
+ import Tasks from "./components/Tasks";
+ import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "learn react",
+      day: "today",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "learn typescript",
+      day: "monday",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "learn photoshop",
+      day: "someday",
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className='container'>
       <Header/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
